6d42a068b1015e5be0fda9d31324499987bcd0ca
anf20 <akhyfajri20@gmail.com>
2026-08-21 22:30:40 +0700

    Allow destructive commands for database resetting on production demo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -48,9 +48,7 @@ class AppServiceProvider extends ServiceProvider
     {
         Date::use(CarbonImmutable::class);
 
-        DB::prohibitDestructiveCommands(
-            app()->isProduction(),
-        );
+        // DB::prohibitDestructiveCommands(app()->isProduction());
 
         Password::defaults(fn (): ?Password => app()->isProduction()
             ? Password::min(12)
